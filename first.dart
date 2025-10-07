0f20b8d847758a8d0baa8f2dcc0a3cf6c10aa904
muhammadharoon303 <haroonafridiharoon303@gmail.com>
2025-10-07 03:34:36 -0700

    Update first.dart

--- first.dart
+++ first.dart
@@ -7,8 +7,4 @@ void main() {
   var full = name + last_name;
   print('The full name is  $name and $last_name is $full');
 }
-if(bool ispresent =true){
-  print("the student is present");
-}else{
-  print("the student is absent");
-}
+
