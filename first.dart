a20127b60d5c4aa839643bc1ce4dbc1a47865d13
muhammadharoon303 <haroonafridiharoon303@gmail.com>
2025-10-07 03:17:21 -0700

    updates

---
+++ first.dart
@@ -0,0 +1,14 @@
+void main() {
+  print('Hello, Dart!');
+
+  // Addition code
+  var name = "Haroon";
+  var last_name = "khan";
+  var full = name + last_name;
+  print('The full name is  $name and $last_name is $full');
+}
+if(bool ispresent =true){
+  print("the student is present");
+}else{
+  print("the student is absent");
+}
