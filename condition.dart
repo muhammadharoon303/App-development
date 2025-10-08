c628f3e36558431e05592f9861c829be087fdf62
muhammadharoon303 <haroonafridiharoon303@gmail.com>
2025-10-07 23:21:15 -0700

    condition program

---
+++ condition.dart
@@ -0,0 +1,11 @@
+void main() {
+  int a = 10;
+  int b = 20;
+  // print("Enter the value of a $a");
+  // // print("Enter the value of b $b");
+  if (a > b) {
+    print("You are eligiable for voting");
+  } else {
+    print("You are not eligiable for voting ");
+  }
+}
