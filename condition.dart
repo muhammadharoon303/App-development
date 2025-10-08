9a55a863bb589d57aece548c77206eb26b9ccd45
muhammadharoon303 <haroonafridiharoon303@gmail.com>
2025-10-07 23:32:35 -0700

    table_for_loop

--- condition.dart
+++ condition.dart
@@ -1,8 +1,6 @@
 void main() {
   int a = 10;
   int b = 20;
-  // print("Enter the value of a $a");
-  // // print("Enter the value of b $b");
   if (a > b) {
     print("You are eligiable for voting");
   } else {
---
+++ table_for_loop.dart
@@ -0,0 +1,7 @@
+void main() {
+  int a = 5;
+  int b = 1;
+  for (a = 5; b <= 10; b++) {
+    print("$a X $b = ${a * b}");
+  }
+}
