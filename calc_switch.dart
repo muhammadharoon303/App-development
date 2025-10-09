a4d5979e9ecad5e18c5d3aa7297c4e3d089e2607
muhammadharoon303 <haroonafridiharoon303@gmail.com>
2025-10-09 01:23:03 -0700

    updated

--- calc_switch.dart
+++ calc_switch.dart
@@ -1,18 +1,21 @@
 import 'dart:io';
 
 void main() {
-  stdout.write("Enter the value of a:");
-  int a = int.parse(stdin.readLineSync()!);
-  String option = stdin.readLineSync()!;
+  stdout.write(
+      "Enter the value of a:"); // stdout.write is used to print in the same line
+  int a = int.parse(stdin
+      .readLineSync()!); // stdin.readLineSync() is used to take input from the user
+  stdout.write("Enter an operator(+,-,*,/)");
+  String operator = stdin.readLineSync()!;
   stdout.write("Enter the value of b:");
   int b = int.parse(stdin.readLineSync()!);
 
-  switch (option) {
+  switch (operator) {
     case '+':
       print("The sum of a and b is ${a + b}");
       break;
     case '-':
-      print("The difference of a and b is ${a - b}");
+      print("The subtraction of a and b is ${a - b}");
       break;
     case '*':
       print("The product of a and b is ${a * b}");
